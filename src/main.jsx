069b0d69710638ff6d2e97d2172733db8fd39273
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-11 15:43:28 +0530

    Fix formatting by removing trailing whitespace in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <GoogleOAuthProvider clientId={CLIENT_ID}>
       <AppRouters />
-    </GoogleOAuthProvider>  
+    </GoogleOAuthProvider>
   </Provider>
 )
